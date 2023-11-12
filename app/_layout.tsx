5d649a082826cd8ff0af47c964ec227e41c27e49
Yuriy Tolstykh <yuriy.tolstykh@gmail.com>
2023-11-11 22:10:36 -0800

    adding font to load

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,4 +1,5 @@
 import FontAwesome from "@expo/vector-icons/FontAwesome";
+import MaterialCommunityIcons from "@expo/vector-icons/MaterialCommunityIcons";
 import {
   DarkTheme,
   DefaultTheme,
@@ -26,6 +27,7 @@ export default function RootLayout() {
   const [loaded, error] = useFonts({
     SpaceMono: require("../assets/fonts/SpaceMono-Regular.ttf"),
     ...FontAwesome.font,
+    ...MaterialCommunityIcons.font,
   });
 
   // Expo Router uses Error Boundaries to catch errors in the navigation tree.
